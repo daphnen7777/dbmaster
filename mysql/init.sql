ecac3d2f4adc08039a1865387d3f83bbfb9dd213
Dogun Kim <121995329+daphnen7777@users.noreply.github.com>
2024-12-11 14:31:25 +0900

    Update init.sql

--- mysql/init.sql
+++ mysql/init.sql
@@ -18,6 +18,22 @@ CREATE TABLE post (
   `reg_date` DATETIME NOT NULL
   ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-INSERT INTO post VALUE(1, 'REPLICATION TEST', 'workergtfo', 'TESTING REPLICATION', '2024-12-09');
+DROP DATABASE IF EXISTS user_management;
+CREATE DATABASE user_management
+       DEFAULT CHARACTER SET utf8mb4
+       DEFAULT COLLATE utf8mb4_unicode_ci;
+
+USE user_management;
+
+CREATE TABLE users (
+       username varchar(50) PRIMARY KEY NOT NULL,
+       password varchar(100) NOT NULL,
+       fullname VARCHAR(100) NOT NULL,
+       email VARCHAR(100) NOT NULL,
+       phone VARCHAR(10),
+       phone2 VARCHAR(10) NOT NULL,
+       phone3 VARCHAR(10) NOT NULL,
+       gender CHAR(1) CHECK(GENDER IN ('m', 'f'))
+);
 
 COMMIT;
